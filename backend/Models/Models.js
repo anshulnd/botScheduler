7fcdb8d8abda0cccc74c496b510405db6bd1bea8
Ethan Oak <eoak@mit.edu>
2018-07-23 10:57:13 -0700

    made required models

--- backend/Models/Models.js
+++ backend/Models/Models.js
@@ -2,12 +2,43 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 //Put schemas here
+//Updates have been made
 
+var taskSchema = new Schema({
+
+})
+
+var meetingSchema = new Schema({
+
+})
+
+var userSchema = new Schema({
+  googleCalenderAccount: {
+    accessToken: ,
+    refreshToken: ,
+    googlePlusprofile:
+  }
+  meetingLength: 30,
+  slackId: String,
+  slackUser: String,
+  slackEmail: String,
+  slackDmids: String
+})
+
+var inviteSchema = new Schema({
+  eventId: String,
+  inviteeId: String,
+  requesterId: String,
+  status: String
+})
+
+var Task = mongoose.model('Task', taskSchema)
 var Meeting = mongoose.model('Meeting', meetingSchema);
 var User = mongoose.model('User', userSchema);
-var Reminder = mongoose.model('Reminder', reminderSchema);
+var Invite = mongoose.model('Invite', inviteSchema);
 module.exports = {
-  User: User,
-  Reminder: Reminder,
+  Task: Task,
   Meeting: Meeting,
+  User: User,
+  Invite: Invite
  };
